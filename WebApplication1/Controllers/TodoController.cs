f1f9fdf0a61dcd49e5b0483d99eaa67766618e37
GET Academy <terje@kolderup.net>
2021-06-15 13:40:11 +0200

    Initial commit

---
+++ WebApplication1/Controllers/TodoController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Model;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private List<Todo> _todos;
+
+        public TodoController(List<Todo> todos)
+        {
+            _todos = todos;
+        }
+
+        [HttpGet]
+        public IEnumerable<Todo> GetTodos()
+        {
+            return _todos;
+        }
+
+        [HttpPost]
+        public void AddNewTodo(Todo todo)
+        {
+            _todos.Add(todo);
+        }
+    }
+}
---
+++ WebApplication1/Controllers/WeatherForecastController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return new WeatherForecast[]
+            {
+                new WeatherForecast {Date =DateTime.Now, Summary = "Get", TemperatureC = 25}
+            };
+        }
+
+        [HttpPost]
+        public IEnumerable<WeatherForecast> Get2()
+        {
+            return new WeatherForecast[]
+            {
+                new WeatherForecast {Date =DateTime.Now, Summary = "Post", TemperatureC = 5}
+            };
+        }
+
+        [HttpPut]
+        public IEnumerable<WeatherForecast> Get3()
+        {
+            return new WeatherForecast[]
+            {
+                new WeatherForecast {Date =DateTime.Now, Summary = "Put", TemperatureC = 5}
+            };
+        }
+    }
+}
